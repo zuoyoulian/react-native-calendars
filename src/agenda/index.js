b2f084fcc981d352e19dfffb3f0a0517b190485e
lekenny <lekenny.cn@gmail.com>
2017-11-13 17:43:00 +0800

    取消日历展开后可以上滑隐藏

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -360,8 +360,8 @@ export default class AgendaView extends Component {
     ];
 
     const shouldAllowDragging = !this.props.hideKnob && !this.state.calendarScrollable;
-    const scrollPadPosition = (shouldAllowDragging ? HEADER_HEIGHT  : this.viewHeight-20) - KNOB_HEIGHT;
-
+    const scrollPadPosition = (shouldAllowDragging ? HEADER_HEIGHT  : 0) - KNOB_HEIGHT;
+    
     const scrollPadStyle = {
       position: 'absolute',
       width: this.viewWidth,
@@ -374,7 +374,7 @@ export default class AgendaView extends Component {
 
     if (!this.props.hideKnob) {
       const knobView = this.props.renderKnob ? this.props.renderKnob() : (<View style={this.styles.knob}/>);
-      knob =  (
+      knob = this.state.calendarScrollable ? null : (
         <View style={[this.styles.knobContainer,{height:shouldAllowDragging?24:60}]}>
           {!shouldAllowDragging&&<View style={{height:10}}/>}
           <View ref={(c) => this.knob = c}>{knobView}</View>
